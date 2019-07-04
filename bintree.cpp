683f29b8f2efeb3663551ece4996d71659439d56
Cameron <Desktop@Desktop.com>
2019-07-03 20:41:29 -0500

    Implemented addNode, getNode, and contains.

--- bintree.cpp
+++ bintree.cpp
@@ -17,7 +17,7 @@ int BinTree::getCount(){
 	return size;
 }
 
-bool BinTree::getRootData(Data *){
+bool BinTree::getRootData(Data * output){
 	if(root) *output = *root; //object slicing
 	else{
 		output->id = -1;
@@ -29,17 +29,51 @@ bool BinTree::getRootData(Data *){
 void BinTree::clear(){
 	
 }
-bool BinTree::addNode(int, string){
-	return false;
+bool BinTree::addNode(int id, string info){
+	DataNode * parent, * search = root;
+	while(search){
+		if(id == search->id) return false;
+		parent = search;
+		search = id < search->id ? search->left : search->right;
+	}
+	
+	DataNode * holder = new DataNode;
+	holder->id = id;
+	holder->information = info;
+	holder->left = holder->right = NULL;
+		
+	if(root) id < parent->id ? parent->left = holder : parent->right = holder;
+	else root = holder;
+	
+	size++;
+	return true;
 }
+
 bool BinTree::removeNode(int){
 	return false;
 }
-bool BinTree::getNode(int, Data *){
+
+//This method is longer since it does not use recursion and contains the work for contains()
+bool BinTree::getNode(int id, Data * output){
+	if(root){
+		DataNode * search = root;
+		while(search){
+			if(search->id == id){
+				if(output) *output = *search;
+				return true;
+			}
+			search = search->id > id ? search->left : search->right;
+		}
+	}
+	if(output){
+		output->id = -1;
+		output->information = "";
+	}
 	return false;
 }
-bool BinTree::contains(int){
-	return false;
+
+bool BinTree::contains(int id){
+	return getNode(id);
 }
 bool BinTree::getHeight(){
 	return false;
